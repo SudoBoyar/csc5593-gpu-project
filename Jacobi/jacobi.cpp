4790997353d6023516f224b5bca4ff8bbdbc3da4
Alex <s.alex.klein@gmail.com>
2017-03-26 22:21:45 -0600

    Changed borders to match paper (i.e. 1s on all edges, not just two sides). Simplified indexes to x, y, and z instead of shifting i, j, and k. Rewrote initialization methods to be somewhat more explicit and in line with x, y, and z indices. Simplified allocation size calculation to avoid needing math import.

--- Jacobi/jacobi.cpp
+++ Jacobi/jacobi.cpp
@@ -5,7 +5,6 @@
 #include <ctype.h>
 #include <errno.h>
 #include <iostream>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -20,7 +19,7 @@ bool debug = false;
 bool sequential = false;
 
 // Data attributes
-int size = 1024, dimensions = 2;
+int size = 1024, dimensions = 2, alloc_size;
 // Run attributes
 int gridSize = 1, blockSize = 16, iterations = 1000;
 
@@ -87,6 +86,13 @@ bool parse_arguments(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    if (dimensions == 1) {
+        alloc_size = size;
+    } else if (dimensions == 2) {
+        alloc_size = size * size;
+    } else {
+        alloc_size = size * size * size;
+    }
 
     return true;
 }
@@ -96,11 +102,11 @@ bool parse_arguments(int argc, char *argv[]) {
 void jacobi1d_sequential(float *data, float *temp) {
     float *swap;
     for (int t = 0; t < iterations; t++) {
-        for (int i = 1; i < size - 1; i++) {
+        for (int x = 1; x < size - 1; x++) {
             // Include Self
-            //temp[i] = (data[i] + data[i - 1] + data[i + 1]) / 3;
+            temp[x] = (data[x] + data[x - 1] + data[x + 1]) / 3;
             // Exclude Self
-            temp[i] = (data[i - 1] + data[i + 1]) / 2;
+            //temp[x] = (data[x - 1] + data[x + 1]) / 2;
         }
 
         swap = data;
@@ -112,25 +118,25 @@ void jacobi1d_sequential(float *data, float *temp) {
 void jacobi2d_sequential(float *data, float *temp) {
     float *swap;
     for (int t = 0; t < iterations; t++) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 1; j < size - 1; j++) {
+        for (int y = 1; y < size - 1; y++) {
+            for (int x = 1; x < size - 1; x++) {
                 // Include Self
-                //temp[i * size + j] =
-                //    (
-                //        data[i * size + j] +
-                //        data[min(i - 1, 0) * size + j] +
-                //        data[max(i + 1, size - 1) * size + j] +
-                //        data[i * size + j - 1] +
-                //        data[i * size + j + 1]
-                //    ) / 5;
-                // Exclude Self
-                temp[i * size + j] =
+                temp[y * size + x] =
                     (
-                        data[max(i - 1, 0) * size + j] +
-                        data[min(i + 1, size - 1) * size + j] +
-                        data[i * size + j - 1] +
-                        data[i * size + j + 1]
-                    ) / 4;
+                        data[y * size + x] +
+                        data[(y - 1) * size + x] +
+                        data[(y + 1) * size + x] +
+                        data[y * size + x - 1] +
+                        data[y * size + x + 1]
+                    ) / 5;
+                // Exclude Self
+                //temp[y * size + x] =
+                //    (
+                //        data[(y - 1) * size + x] +
+                //        data[(y + 1) * size + x] +
+                //        data[y * size + x - 1] +
+                //        data[y * size + x + 1]
+                //    ) / 4;
             }
         }
 
@@ -143,30 +149,30 @@ void jacobi2d_sequential(float *data, float *temp) {
 void jacobi3d_sequential(float *data, float *temp) {
     float *swap;
     for (int t = 0; t < iterations; t++) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 1; k < size - 1; k++) {
+        for (int z = 1; z < size - 1; z++) {
+            for (int y = 1; y < size - 1; y++) {
+                for (int x = 1; x < size - 1; x++) {
                     // Include Self
-                    //temp[i * size * size + j * size + k] =
-                    //    (
-                    //        data[i * size * size + j * size + k] +
-                    //        data[max(i - 1, 0) * size * size + j * size + k] +
-                    //        data[min(i + 1, size - 1) * size * size + j * size + k] +
-                    //        data[i * size * size + min(j - 1, 0) * size + k] +
-                    //        data[i * size * size + max(j + 1, size - 1) * size + k] +
-                    //        data[i * size * size + j * size + k - 1] +
-                    //        data[i * size * size + j * size + k + 1]
-                    //    ) / 7;
-                    // Exclude Self
-                    temp[i * size * size + j * size + k] =
+                    temp[z * size * size + y * size + x] =
                         (
-                            data[max(i - 1, 0) * size * size + j * size + k] +
-                            data[min(i + 1, size - 1) * size * size + j * size + k] +
-                            data[i * size * size + (j - 1) * size + k] +
-                            data[i * size * size + (j + 1) * size + k] +
-                            data[i * size * size + j * size + k - 1] +
-                            data[i * size * size + j * size + k + 1]
-                        ) / 6;
+                            data[z * size * size + y * size + x] +
+                            data[(z - 1) * size * size + y * size + x] +
+                            data[(z + 1) * size * size + y * size + x] +
+                            data[z * size * size + (y - 1) * size + x] +
+                            data[z * size * size + (y + 1) * size + x] +
+                            data[z * size * size + y * size + x - 1] +
+                            data[z * size * size + y * size + x + 1]
+                        ) / 7;
+                    // Exclude Self
+                    //temp[z * size * size + y * size + x] =
+                    //    (
+                    //        data[(z - 1) * size * size + y * size + x] +
+                    //        data[(z + 1) * size * size + y * size + x] +
+                    //        data[z * size * size + (y - 1) * size + x] +
+                    //        data[z * size * size + (y + 1) * size + x] +
+                    //        data[z * size * size + y * size + x - 1] +
+                    //        data[z * size * size + y * size + x + 1]
+                    //    ) / 6;
                 }
             }
         }
@@ -201,33 +207,63 @@ void initialize_data_1d(float *data) {
 }
 
 void initialize_data_2d(float *data) {
+    int x, y;
+
     for (int i = 0; i < size; i++) {
         // Top Row
-        //data[i] = 1.0;
+        x = i;
+        y = 0;
+        data[y * size + x] = 1.0;
         // Bottom Row
-        //data[(size - 1) * size + i] = 1.0;
+        x = i;
+        y = size - 1;
+        data[y * size + x] = 1.0;
         // Left Column
-        data[i * size] = 1.0;
+        x = 0;
+        y = i;
+        data[y * size + x] = 1.0;
         // Right Column
-        data[i * size + size - 1] = 1.0;
+        x = size - 1;
+        y = i;
+        data[y * size + x] = 1.0;
     }
 }
 
 void initialize_data_3d(float *data) {
+    int x, y, z;
+
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             // Face 1: X = 0 Plane
-            data[i * size + j] = 1.0;
+            x = 0;
+            y = j;
+            z = i;
+            data[z * size * size + y * size + x] = 1.0;
             // Face 2: X = N Plane
-            data[(size - 1) * size * size + i * size + j] = 1.0;
+            x = size - 1;
+            y = j;
+            z = i;
+            data[z * size * size + y * size + x] = 1.0;
             // Face 3: Y = 0 Plane
-            //data[i * size * size + j] = 1.0;
+            x = j;
+            y = 0;
+            z = i;
+            data[z * size * size + y * size + x] = 1.0;
             // Face 4: Y = N Plane
-            //data[i * size * size + (size - 1) * size + j] = 1.0;
+            x = j;
+            y = size - 1;
+            z = i;
+            data[z * size * size + y * size + x] = 1.0;
             // Face 5: Z = 0 Plane
-            //data[i * size * size + j * size] = 1.0;
+            x = j;
+            y = i;
+            z = 0;
+            data[z * size * size + y * size + x] = 1.0;
             // Face 6: Z = N Plane
-            //data[i * size * size + j * size + size - 1] = 1.0;
+            x = j;
+            y = i;
+            z = size - 1;
+            data[z * size * size + y * size + x] = 1.0;
         }
     }
 }
@@ -249,28 +285,28 @@ void initialize_data(float *data) {
 
 // Debug Helpers
 
-void print_matrix(float *data) {
-    if (size > 10) {
-        cerr << "Matrix too big to print\n" << endl;
+void print_data(float *data) {
+    if (size > 13) {
+        cerr << "Data too big to print\n" << endl;
         return;
     }
 
     if (dimensions == 1) {
-        for (int i = 0; i < size; i++) {
-            fprintf(stdout, "%.3f ", data[i]);
+        for (int x = 0; x < size; x++) {
+            fprintf(stdout, "%.3f ", data[x]);
         }
     } else if (dimensions == 2) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                fprintf(stdout, "%.3f ", data[i * size + j]);
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                fprintf(stdout, "%.3f ", data[y * size + x]);
             }
             cout << endl;
         }
     } else if (dimensions == 3) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    fprintf(stdout, "%.3f ", data[i * size * size + j * size + k]);
+        for (int z = 0; z < size; z++) {
+            for (int y = 0; y < size; y++) {
+                for (int x = 0; x < size; x++) {
+                    fprintf(stdout, "%.3f ", data[z * size * size + y * size + x]);
                 }
                 cout << endl;
             }
@@ -285,17 +321,17 @@ void print_matrix(float *data) {
 
 int main(int argc, char *argv[]) {
     parse_arguments(argc, argv);
-    float *data = new float[(int) pow(size, dimensions)];
-    float *temp = new float[(int) pow(size, dimensions)];
+    float *data = new float[alloc_size];
+    float *temp = new float[alloc_size];
 
     initialize_data(data);
     initialize_data(temp);
 
-    if (debug) { print_matrix(data); }
+    if (debug) { print_data(data); }
     if (sequential) {
         jacobi_sequential(data, temp);
     } else {
         // Add CUDA calls
     }
-    if (debug) { print_matrix(data); }
+    if (debug) { print_data(data); }
 }
