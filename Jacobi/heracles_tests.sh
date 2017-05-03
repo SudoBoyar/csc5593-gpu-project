21396295250411a8b44b815c3b46d9ef400e6885
walker <kirk.walker@cu.edu>
2017-05-02 23:31:11 -0600

    Changed 255 to 256

--- Jacobi/heracles_tests.sh
+++ Jacobi/heracles_tests.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Blocked Algorithm run for a data block of size 256x256x255"
+echo "Blocked Algorithm run for a data block of size 256x256x256"
 ssh node18 $DIR/blocked_gpu -d 3 -n 256 -i 16 
 ssh node18 $DIR/blocked_gpu -d 3 -n 256 -i 32 
 ssh node18 $DIR/blocked_gpu -d 3 -n 256 -i 64
@@ -16,7 +16,7 @@ ssh node18 $DIR/blocked_gpu -d 3 -n 512 -i 64
 ssh node18 $DIR/blocked_gpu -d 3 -n 512 -i 128
 ssh node18 $DIR/blocked_gpu -d 3 -n 512 -i 256
 
-echo "Naive Jacobi 3d Algorithm run for a data block of size 256x256x255"
+echo "Naive Jacobi 3d Algorithm run for a data block of size 256x256x256"
 ssh node18 $DIR/naive_gpu -d 3 -n 256 -i 16 
 ssh node18 $DIR/naive_gpu -d 3 -n 256 -i 32 
 ssh node18 $DIR/naive_gpu -d 3 -n 256 -i 64
@@ -30,7 +30,7 @@ ssh node18 $DIR/naive_gpu -d 3 -n 512 -i 64
 ssh node18 $DIR/naive_gpu -d 3 -n 512 -i 128
 ssh node18 $DIR/naive_gpu -d 3 -n 512 -i 256
 
-echo "Cached Plane w/o Shared Memory Algorithm run for a data block of size 256x256x255"
+echo "Cached Plane w/o Shared Memory Algorithm run for a data block of size 256x256x256"
 ssh node18 $DIR/cps_gpu -d 3 -n 256 -i 16 
 ssh node18 $DIR/cps_gpu -d 3 -n 256 -i 32 
 ssh node18 $DIR/cps_gpu -d 3 -n 256 -i 64
@@ -44,7 +44,7 @@ ssh node18 $DIR/cps_gpu -d 3 -n 512 -i 64
 ssh node18 $DIR/cps_gpu -d 3 -n 512 -i 128
 ssh node18 $DIR/cps_gpu -d 3 -n 512 -i 256
 
-echo "Cached Plane with Shared Memory Algorithm run for a data block of size 256x256x255"
+echo "Cached Plane with Shared Memory Algorithm run for a data block of size 256x256x256"
 ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 16 
 ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 32 
 ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 64
@@ -58,7 +58,7 @@ ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 64
 ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 128
 ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 256
 
-echo "Overlapped Tiling Algorithm run for a data block of size 256x256x255"
+echo "Overlapped Tiling Algorithm run for a data block of size 256x256x256"
 ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 16 
 ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 32 
 ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 64
