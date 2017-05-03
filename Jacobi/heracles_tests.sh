7fd66ad76365c60e30f42a75c553dbdfa0f60ddd
kirkjwalker@yahoo.com <kirkjwalker@yahoo.com>
2017-05-02 22:49:13 -0600

    adding the rest of the tests

--- Jacobi/heracles_tests.sh
+++ Jacobi/heracles_tests.sh
@@ -2,6 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Blocked Algorithm run for a data block of size 256x256x255"
+echo "# of iterations: 16"
+echo "# of iterations: 32"
 ssh node18 $DIR/blocked_gpu -d 3 -n 256 -i 16 
 ssh node18 $DIR/blocked_gpu -d 3 -n 256 -i 32 
 ssh node18 $DIR/blocked_gpu -d 3 -n 256 -i 64
@@ -37,3 +40,27 @@ ssh node18 $DIR/cps_gpu -d 3 -n 512 -i 32
 ssh node18 $DIR/cps_gpu -d 3 -n 512 -i 64
 ssh node18 $DIR/cps_gpu -d 3 -n 512 -i 128
 ssh node18 $DIR/cps_gpu -d 3 -n 512 -i 256
+
+ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 16 
+ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 32 
+ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 64
+ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 128
+ssh node18 $DIR/cpssm_gpu -d 3 -n 256 -i 256
+
+ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 16 
+ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 32 
+ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 64
+ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 128
+ssh node18 $DIR/cpssm_gpu -d 3 -n 512 -i 256
+
+ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 16 
+ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 32 
+ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 64
+ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 128
+ssh node18 $DIR/overlapped_gpu -d 3 -n 256 -i 256
+
+ssh node18 $DIR/overlapped_gpu -d 3 -n 512 -i 16 
+ssh node18 $DIR/overlapped_gpu -d 3 -n 512 -i 32 
+ssh node18 $DIR/overlapped_gpu -d 3 -n 512 -i 64
+ssh node18 $DIR/overlapped_gpu -d 3 -n 512 -i 128
+ssh node18 $DIR/overlapped_gpu -d 3 -n 512 -i 256
