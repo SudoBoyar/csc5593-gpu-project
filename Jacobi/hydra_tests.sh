df3905784832cca309d6b79fad9ffec0df71ea89
walker <kirk.walker@cu.edu>
2017-05-02 23:34:09 -0600

    255-> 256

--- Jacobi/hydra_tests.sh
+++ Jacobi/hydra_tests.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Blocked Algorithm run for a data block of size 256x256x255"
+echo "Blocked Algorithm run for a data block of size 256x256x256"
 bpsh 12 $DIR/blocked_gpu -d 3 -n 256 -i 16 
 bpsh 12 $DIR/blocked_gpu -d 3 -n 256 -i 32 
 bpsh 12 $DIR/blocked_gpu -d 3 -n 256 -i 64
@@ -16,7 +16,7 @@ bpsh 12 $DIR/blocked_gpu -d 3 -n 512 -i 64
 bpsh 12 $DIR/blocked_gpu -d 3 -n 512 -i 128
 bpsh 12 $DIR/blocked_gpu -d 3 -n 512 -i 256
 
-echo "Naive Jacobi 3d Algorithm run for a data block of size 256x256x255"
+echo "Naive Jacobi 3d Algorithm run for a data block of size 256x256x256"
 bpsh 12 $DIR/naive_gpu -d 3 -n 256 -i 16 
 bpsh 12 $DIR/naive_gpu -d 3 -n 256 -i 32 
 bpsh 12 $DIR/naive_gpu -d 3 -n 256 -i 64
@@ -30,7 +30,7 @@ bpsh 12 $DIR/naive_gpu -d 3 -n 512 -i 64
 bpsh 12 $DIR/naive_gpu -d 3 -n 512 -i 128
 bpsh 12 $DIR/naive_gpu -d 3 -n 512 -i 256
 
-echo "Cached Plane w/o Shared Memory Algorithm run for a data block of size 256x256x255"
+echo "Cached Plane w/o Shared Memory Algorithm run for a data block of size 256x256x256"
 bpsh 12 $DIR/cps_gpu -d 3 -n 256 -i 16 
 bpsh 12 $DIR/cps_gpu -d 3 -n 256 -i 32 
 bpsh 12 $DIR/cps_gpu -d 3 -n 256 -i 64
@@ -44,7 +44,7 @@ bpsh 12 $DIR/cps_gpu -d 3 -n 512 -i 64
 bpsh 12 $DIR/cps_gpu -d 3 -n 512 -i 128
 bpsh 12 $DIR/cps_gpu -d 3 -n 512 -i 256
 
-echo "Cached Plane with Shared Memory Algorithm run for a data block of size 256x256x255"
+echo "Cached Plane with Shared Memory Algorithm run for a data block of size 256x256x256"
 bpsh 12 $DIR/cpssm_gpu -d 3 -n 256 -i 16 
 bpsh 12 $DIR/cpssm_gpu -d 3 -n 256 -i 32 
 bpsh 12 $DIR/cpssm_gpu -d 3 -n 256 -i 64
@@ -58,7 +58,7 @@ bpsh 12 $DIR/cpssm_gpu -d 3 -n 512 -i 64
 bpsh 12 $DIR/cpssm_gpu -d 3 -n 512 -i 128
 bpsh 12 $DIR/cpssm_gpu -d 3 -n 512 -i 256
 
-echo "Overlapped Tiling Algorithm run for a data block of size 256x256x255"
+echo "Overlapped Tiling Algorithm run for a data block of size 256x256x256"
 bpsh 12 $DIR/overlapped_gpu -d 3 -n 256 -i 16 
 bpsh 12 $DIR/overlapped_gpu -d 3 -n 256 -i 32 
 bpsh 12 $DIR/overlapped_gpu -d 3 -n 256 -i 64
